a47d5277cb101a6ba7254f0188edcf7cc33de71d
Manuel F. Del Toro <mdeltoro@belatrixsf.com>
2019-07-23 10:49:22 -0500

    single-spa added to the root project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { registerApplication, start } from "single-spa";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+registerApplication(
+  'root',          // Name of this single-spa application
+  () => import("./root.app"), // Our loading function
+  () => true // Our activity function
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+start();
+
 serviceWorker.unregister();
